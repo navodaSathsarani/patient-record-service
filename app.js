69eb8dba198083e75d93378a67a4fba52e9d8e8a
Navoda Sathsarani <navoda.sathsarani@axiatadigitallabs.com>
2024-12-20 10:04:22 +0530

    Added Dockerfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30001;
 
 // Middleware
 app.use(bodyParser.json());
